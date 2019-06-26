cf2de0bb69d0da05b5c301c07941969be985a595
Patricio Palladino <email@patriciopalladino.com>
2019-06-25 21:05:33 -0300

    Use node's implementation if available

--- node.js
+++ node.js
@@ -1,7 +1,13 @@
-var scrypt = require('scrypt')
+const crypto = require("crypto");
 
-function hash (key, salt, n, r, p, dklen, progressCb) {
-  return scrypt.hashSync(key, { N: n, r: r, p: p }, dklen, salt)
+if (crypto.scryptSync === undefined) {
+  module.exports = require("./js");
+  return
 }
 
-module.exports = hash
+function hash(key, salt, n, r, p, dklen, progressCb) {
+  const maxmem = 2 * 128 * n * r; // See Node's doc for an explanation
+  return crypto.scryptSync(key, salt, dklen, { N: n, r, p, maxmem });
+}
+
+module.exports = hash;
