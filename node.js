1f962856cbe6b18bebd419cd6e8394764a975991
Patricio Palladino <email@patriciopalladino.com>
2019-06-25 21:21:53 -0300

    Standard formatting

--- node.js
+++ node.js
@@ -1,13 +1,12 @@
-const crypto = require("crypto");
+const crypto = require('crypto')
+
+function hash (key, salt, n, r, p, dklen, progressCb) {
+  const maxmem = 2 * 128 * n * r // See Node's doc for an explanation
+  return crypto.scryptSync(key, salt, dklen, { N: n, r, p, maxmem })
+}
 
 if (crypto.scryptSync === undefined) {
-  module.exports = require("./js");
-  return
+  module.exports = require('./js')
+} else {
+  module.exports = hash
 }
-
-function hash(key, salt, n, r, p, dklen, progressCb) {
-  const maxmem = 2 * 128 * n * r; // See Node's doc for an explanation
-  return crypto.scryptSync(key, salt, dklen, { N: n, r, p, maxmem });
-}
-
-module.exports = hash;
